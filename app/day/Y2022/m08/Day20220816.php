fa7dbb672ade129f7d9215cfdc132ebb4e29e088
RickChen13 <rickchen13@gmail.com>
2022-08-30 18:59:38 +0800

    Update Day20220816.php

--- app/day/Y2022/m08/Day20220816.php
+++ app/day/Y2022/m08/Day20220816.php
@@ -13,32 +13,6 @@ class Day20220816
      */
     public function run()
     {
-        $arr0 = [1, 2, 3, 4];
-        $arr1 = [5, 6, 7, 8];
-        $arr2 = [9, 10, 11, 12];
-        $result = $this->concat($arr0, $arr1, $arr2);
-        var_dump($result);
-    }
-
-    /**
-     * 数组拼接
-     *
-     * @param array $arr0
-     * @param array $arr1
-     * @param array ...$arrs
-     * @return array
-     * @tag 数组 数组拼接 js的concat函数实现
-     */
-    public function concat($arr0, $arr1, ...$arrs)
-    {
-        array_walk($arr1, function ($v) use (&$arr0) {
-            $arr0[] = $v;
-        });
-        foreach ($arrs as $arr) {
-            array_walk($arr, function ($v) use (&$arr0) {
-                $arr0[] = $v;
-            });
-        }
-        return $arr0;
+        echo "hello,world!", PHP_EOL;
     }
 }
